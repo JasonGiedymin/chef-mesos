0435f672d62c56abc9009d65d8f2dce7ca7cedb7
Jason Giedymin <jasong@apache.org>
2013-12-05 08:37:52 -0500

    Add attributes, cookbook actions

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,21 +1,23 @@
-def packageType()
-  case node[:platform]
-    when 'debian','ubuntu'
-      'deb'
-    else
-      'rpm'
-  end
+case node[:platform]
+  when 'debian','ubuntu'
+    default['mesos']['package_format'] = 'deb'
+  else
+    default['mesos']['package_format'] = 'rpm'
 end
 
 # misc attribs
 default['mesos']['install']['force']         = true # force the install
 
 # packaging
-default['mesos']['install']['from_package']  = true # flag to declare if install should start from a pre- packaged binary
+default['mesos']['install']['method']        = pkg # {src|pkg|pkgsrc}
+
 default['mesos']['install']['pkg_ver']       = '0.14.2'
 default['mesos']['install']['pkg_arch']      = 'amd64' # right now that is all you get
-default['mesos']['install']['pkg_url']       = "http://downloads.mesosphere.io/master/#{node[:platform]}/#{node[:platform_version]}/mesos_#{default['install']['pkg_ver']}_#{default['install']['pkg_ver']}.#{packageType()}"
+
+default['mesos']['install']['filename']      = "mesos_#{default[:mesos][:install][:pkg_ver]}_#{default[:mesos][:install][:pkg_arch]}.#{default[:mesos][:package_format]}"
+default['mesos']['install']['pkg_url']       = "http://downloads.mesosphere.io/master/#{node[:platform]}/#{node[:platform_version]}/#{node[:mesos][:install][:filename]}"
 
 # compile
-default['mesos']['compile']['dir']           = '~/temp' # temporary dir to unpack/compile
-default['mesos']['compile']['branch']        = 'master' # git branch to compile from
+default['mesos']['source']['dir']           = "#{Chef::Config[:file_cache_path]}/mesos" # temporary dir to unpack/compile
+default['mesos']['source']['repo']          = 'https://git-wip-us.apache.org/repos/asf/mesos.git'
+default['mesos']['source']['branch']        = 'master' # git branch to compile from
--- recipes/default.rb
+++ recipes/default.rb
@@ -21,3 +21,84 @@ package 'python-setuptools'
 # service 'mesos-slave' do
 #   action [:enable, :stop]
 # end
+
+packageFile = "#{node.default.mesos.source.dir}/#{node.default.mesos.install.filename}"
+
+def checkWorkingDir()
+  directory node.mesos.source.dir do
+    # owner "vagrant"
+    # group "vagrant"
+    mode 00644
+    action :create
+  end
+end
+
+def checkForPkgFile()
+  remote_file packageFile do
+    source node.default.mesos.install.pkg_url
+    # owner 'vagrant'
+    # group 'vagrant'
+    mode 00644
+    action :create_if_missing
+  end
+end
+
+def installPkg() {
+  dpkg_package packageFile do
+    action :install
+  end
+}
+
+
+def prepareSrc()
+  git node.mesos.source.dir do
+    repository node.mesos.source.repo
+    reference node.mesos.source.branch
+    action :sync
+  end
+
+  bash "configure_mesos" do
+    cwd: node.mesos.source.dir
+    code <<-EOH
+      ./configure
+    EOH
+  end
+end
+
+def compileSrc()
+  bash "compile_mesos" do
+    cwd: node.mesos.source.dir
+    code <<-EOH
+      ./configure
+      make clean
+      make
+      sudo make uninstall
+      sudo make install
+    EOH
+  end
+end
+
+# Separated so we one may debug if necessary
+def installSrc()
+  bash "install_mesos" do
+    cwd: node.mesos.source.dir
+    code <<-EOH
+      sudo make uninstall
+      sudo make install
+    EOH
+  end
+end
+
+checkWorkingDir()
+
+case node.mesos.install.method
+when :pkg
+  checkForPkgFile()
+  installPkg()
+when :pkgsrc
+  # not yet ready
+when :src
+  prepareSrc()
+  compileSrc()
+  instalSrc()
+end
